4af2e65e45ab714f4a4265ce7df039da3b1220c8
ParadoX <djei007@mail.ru>
2023-07-05 18:41:38 +0300

    JavaMili

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost = 18200;
+        int bonus = 20;
+        int mil = cost/ bonus;
+        System.out.println(mil);
+    }
+}
