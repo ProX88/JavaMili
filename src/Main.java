5f9cd1e9cf20302178daf4b12db90cd5c4cdd18c
ParadoX <djei007@mail.ru>
2023-07-13 12:22:52 +0300

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int cost = 18200;
         int bonus = 20;
-        int mil = cost/ bonus;
+        int mil = cost / bonus;
         System.out.println(mil);
     }
 }
